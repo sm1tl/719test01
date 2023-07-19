436b31a032d68bcd951a4d60232216adade50273
sm1tl <itscaswilliams@hotmail.com>
2023-07-19 16:45:06 +0800

    jkbsfkbusgr

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <session class="border session">
+  section
+  
+  <aside class="border aside left-aside">
+    left-aside
+  </aside>
+  <aside class="border aside right-aside">
+    right-aside
+  </aside>
+  <footer class="border bottom">
+    footer
+  </footer>
+</session>
+    </div>
+  );
+}
+
+export default App;
